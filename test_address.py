25cba29ec66811c3e3a88954a5558a3afd222929
Jonathan David Livesay <jonl316@gmail.com>
2019-10-02 11:35:19 -0600

    Finished multiple tests for the adress class

--- test_address.py
+++ test_address.py
@@ -9,3 +9,32 @@ def address():
 
 def test_address_creation(address):
     assert 'Sachse' == address.city
+
+
+def test_address_street1(address):
+    assert address.street1 == '6512 Briar Lake Trl'
+
+
+def test_address_street2(address):
+    assert address.street2 == 'Room 123'
+
+
+def test_address_state(address):
+    assert address.state == 'TX'
+
+
+def test_address_zip_code(address):
+    assert address.zip_code == '75048'
+    assert type(address.zip_code) != int
+
+
+def test_address_is_not_None_type(address):
+    assert address is not None
+
+
+def test_address_is_not_blank(address):
+    assert address.street1 is not ''
+    assert address.street2 is not ''
+    assert address.city is not ''
+    assert address.state is not ''
+    assert address.zip_code is not ''
