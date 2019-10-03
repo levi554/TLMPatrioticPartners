950c3770b741d34e698ef32a289a39302a058412
Jonathan David Livesay <jonl316@gmail.com>
2019-10-02 23:31:00 -0600

    Added and passed tests for the phone number

--- test_phone_number.py
+++ test_phone_number.py
@@ -3,7 +3,29 @@ from contact import PhoneNumber, WorkPhoneNumber, HomePhoneNumber, OtherPhoneNum
 
 @pytest.fixture
 def phone():
-    phone_number = PhoneNumber('469.515.9958')
-    work_number = WorkPhoneNumber('469.515.1111')
-    home_number = HomePhoneNumber('469.515.2222')
-    other_number = OtherPhoneNumber('469.515.3333')
+    phone = PhoneNumber('469.515.1111')
+    return phone
+
+@pytest.fixture
+def work_phone():
+    work_phone = WorkPhoneNumber('469.515.2222')
+    return work_phone
+
+@pytest.fixture
+def home_phone():
+    home_phone = HomePhoneNumber('469.515.3333')
+    return home_phone
+
+@pytest.fixture
+def other_phone():
+    other_phone = OtherPhoneNumber('469.515.4444')
+    return other_phone
+
+
+def test_phone_number(phone):
+    assert phone.phone_number == '469.515.1111'
+
+
+def test_phone_number_id_not_blank(phone):
+    assert phone.phone_number_id is not None
+    assert phone.phone_number_id != ''
