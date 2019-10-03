18f0c941958ee2cd0d108a5966401ed528913f04
Jonathan David Livesay <jonl316@gmail.com>
2019-10-03 11:37:47 -0600

    Adding pytest fixtures to the test_contact.py file to make testing them easier.

--- test_contact.py
+++ test_contact.py
@@ -1,22 +1,34 @@
+import pytest
 from contact import Person, Contact, WorkPhoneNumber, HomePhoneNumber, OtherPhoneNumber
 
 
-def test_creation_of_person():
-    person1 = Person('Jonathan', 'Livesay')
-    person2 = Person('Amber', '')
+@pytest.fixture
+def person1():
+    p1 = Person('Jonathan', 'Livesay')
+    return p1
 
+@pytest.fixture
+def person2():
+    p2 = Person('Amber', 'Livesay')
+    return p2
+
+@pytest.fixture
+def simple_contact():
+    sc = Contact('Jonathan', 'Livesay', None, None, None)
+    return sc
+
+
+def test_creation_of_person(person1, person2):
     assert person1.first_name == 'Jonathan'
     assert person2.first_name == 'Amber'
-    assert person2.last_name == ''
+    assert person2.last_name == 'Livesay'
     assert person1.last_name == 'Livesay'
 
 
-def test_creation_of_simple_contact():
-    contact1 = Contact('Jonathan', 'Livesay')
-
-    assert contact1.first_name == 'Jonathan'
-    assert contact1.phone_numbers is not None
-    assert contact1.phone_numbers == []
+def test_creation_of_simple_contact(simple_contact):
+    assert simple_contact.first_name == 'Jonathan'
+    assert simple_contact.phone_numbers is not None
+    assert simple_contact.phone_numbers == []
 
 
 def test_work_phone_number_in_contact():
